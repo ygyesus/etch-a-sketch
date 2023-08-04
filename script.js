1b7f782fcc2b04df2c01c19ddcd00cdbb969f175
Yosef Gebreyesus <withstunna005@gmail.com>
2023-08-04 16:21:49 +0300

    Add top button, enhance hover, and fix grid
    The hover effect now works only when mouse is dragged (IE clicked or
    moved while clicking)
    
    Grid was previously 4x4, now changed to 16x16. Also, grid style changed

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 const container = document.querySelector('.container');
-
-for (let i = 0; i < 16; i++){
+let dragging = false;
+for (let i = 0; i < 256; i++){
     const gridMember = document.createElement('div');
     gridMember.classList.add('gridMember');
-    gridMember.addEventListener('mouseover',()=>{
+
+    gridMember.addEventListener('mousedown', ()=>{                  // click vs mousedown          
         gridMember.classList.add('trail');
-    })
-    
+        dragging = true;
+    });
+
+    gridMember.addEventListener('mouseover',()=>{
+        if (dragging) 
+            gridMember.classList.add('trail');
+    });
+
+    gridMember.addEventListener('mouseup', ()=>{
+        dragging = false;
+    });
+
     container.appendChild(gridMember);
 }
 
+const promptUser = function(){
+    let height;
+    let width;
+    height = prompt("Enter height");
+    if (height){
+        if (height > 100){
+
+            alert("Maximum value is 100");
+            promptUser();
+            
+        }
+        width = prompt("Enter width");
+        if (height > 100 || width > 100){
+
+            alert("Maximum value is 100");
+            promptUser();    
+        }
+    }
+
+    return [height, width];
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', ()=>{
+    var array = promptUser();
+    alert(array);
+})
